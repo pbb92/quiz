04fd30d3978324f1d5281f3741fc005b95e9d112
pbb92 <pablo_kss@hotmail.com>
2016-05-03 23:38:28 +0200

    Tema 9: Postgres en Heroku

--- models/index.js
+++ models/index.js
@@ -3,8 +3,20 @@ var path = require('path');
 // Cargar ORM
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(null, null, null, 
-	 				{ dialect: "sqlite", storage: "quiz.sqlite" });
+var url, storage;
+
+if(!process.env.DATABASE_URL){
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+} else{
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, 
+	 				{ storage: storage,
+                      omitNull: true
+                    });
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
