dd2b6bb59a1f5f3e73f024298b5126d3fc0bcc0f
pbb92 <pablo_kss@hotmail.com>
2016-05-04 16:22:44 +0200

    Tema 10: lista de preguntas

--- models/index.js
+++ models/index.js
@@ -29,9 +29,10 @@ sequelize.sync()
         return Quiz.count()
                 .then(function (c) {
                     if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.create({ question: 'Capital de Italia',
-          	                                 answer: 'Roma'
-          	                               })
+                        return Quiz
+                            .bulkCreate([ { question: 'Capital de Italia',      answer: 'Roma' },
+                                          { question: 'Capital de Portugal',    answer: 'Lisboa'}
+                                        ])
                                    .then(function() {
                                         console.log('Base de datos inicializada con datos');
                                     });
