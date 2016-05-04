c8e453528e51c5415bc316e32d4eec9d91c645af
pbb92 <pablo_kss@hotmail.com>
2016-05-04 17:14:44 +0200

    Tema 11: autoload

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,9 @@ router.get('/author', function(req, res, next){
 	res.render('author');
 });
 
+// Autoload de rutas que usen :quizId
+router.param('quizId', quizController.load);
+
 // Definición de rutas /quizzes
 
 router.get('/quizzes',							quizController.index);
