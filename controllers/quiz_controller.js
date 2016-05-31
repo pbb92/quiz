6ca1a4006a96a8567982a7782130aacc0f5f8ea0
pbb92 <pablo_kss@hotmail.com>
2016-05-31 16:08:31 +0200

    Tema 14

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,5 @@
 var models = require('../models');
+var Sequelize = require('sequelize');
 
 //Autoload el quiz asociado a :quizId
 
@@ -77,6 +78,15 @@ exports.create = function(req, res, next) {
 		.then(function(quiz) {
 			req.flash('success', 'Quiz creado con éxito.');
 			res.redirect('/quizzes');
+		})
+		.catch(Sequelize.ValidationError, function(error) {
+
+			req.flash('error', 'Errores en el formulario');
+			for (var i in error.errors) {
+				req.flash('error', error.errors[i].value);
+			};
+
+			res.render('quizzes/new', {quiz: quiz});
 		})
 		.catch(function(error) {
 			req.flash('error', 'Error al crear un Quiz: '+error.message)
