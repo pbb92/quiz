16debfadec2e9c7144342200d6f67ccb6e6c258b
pbb92 <pablo_kss@hotmail.com>
2016-05-31 16:53:27 +0200

    Tema 15

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -92,4 +92,38 @@ exports.create = function(req, res, next) {
 			req.flash('error', 'Error al crear un Quiz: '+error.message)
 			next(error);
 		});
-	};
+};
+
+
+// GET /quizzes/:id/edit
+exports.edit = function(req, res, next) {
+	var quiz = req.quiz;
+
+	res.render('quizzes/edit', {quiz:quiz});	
+};
+
+// PUT /quizzes/:id
+exports.update = function(req, res, next) {
+
+	req.quiz.question = req.body.quiz.question;
+	req.quiz.answer = req.body.quiz.answer;
+
+	req.quiz.save({fields: ["question", "answer"]})
+		.then(function(quiz) {
+		req.flash('success', 'Quiz editado con éxito.');
+			res.redirect('/quizzes');
+		})
+		.catch(Sequelize.ValidationError, function(error) {
+
+			req.flash('error', 'Errores en el formulario');
+			for (var i in error.errors) {
+				req.flash('error', error.errors[i].value);
+			};
+
+			res.render('quizzes/new', {quiz: quiz});
+		})
+		.catch(function(error) {
+			req.flash('error', 'Error al crear un Quiz: '+error.message)
+			next(error);
+		});
+};
