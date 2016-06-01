fa5b29c2d1749ebc92429b5d3c56bea6d8c030b3
pbb92 <pablo_kss@hotmail.com>
2016-06-01 10:45:26 +0200

    Tema 18

--- models/index.js
+++ models/index.js
@@ -21,4 +21,12 @@ var sequelize = new Sequelize(url,
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+// Importar la definicion de la tabla Comments de comment.js
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+// Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment; // exportar definicion de tabla comments
